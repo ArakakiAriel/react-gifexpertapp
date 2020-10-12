3b30d66c7202f92d3721e7d0bf6a2ff004eba3a7
root <root@DESKTOP-T81MJ18.localdomain>
2020-10-12 19:19:35 +0900

    Add docs

---
+++ docs/precache-manifest.13a929a31627338855bd76a8a15a10d8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "626e1cdb975951ad6ff35a9289bf4f42",
+    "url": "/index.html"
+  },
+  {
+    "revision": "292cfffcc572ca966111",
+    "url": "/static/css/main.fc691876.chunk.css"
+  },
+  {
+    "revision": "0e66ef2a9c52ffee85cd",
+    "url": "/static/js/2.5d2d5aae.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.5d2d5aae.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "292cfffcc572ca966111",
+    "url": "/static/js/main.cede82db.chunk.js"
+  },
+  {
+    "revision": "0a62db85d9147c438712",
+    "url": "/static/js/runtime-main.2bd8def6.js"
+  }
+]);
